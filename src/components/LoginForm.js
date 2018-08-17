788406418818e969e60a4f7bbb02d691996053ea
Unknown <luke.towell@hotmail.com>
2018-08-17 15:28:17 +0100

    Initial setup of the actions and reducers
    - Initial set up of the actions and the reducers within the application.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
+import { connect } from 'react-redux';
+import emailChanged from '../actions';
 
 
 class LoginForm extends Component {
+
+    onEmailChange(text){
+        this.props.emailChanged(text)
+    }
+
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input
                         label="E-mail"
-                        placeholder="test@test.com"/>
+                        placeholder="test@test.com"
+                        onChangeText={this.onEmailChange.bind(this)}/>
                 </CardSection>
+
                 <CardSection>
                     <Input
                         label="Password"
                         placeholder="Password"
                         passwordInput/>
                 </CardSection>
+
                 <CardSection>
                     <Button buttonText="Login" />
                 </CardSection>
@@ -26,4 +36,4 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default connect (null, emailChanged, LoginForm);
---
+++ src/reducers/authReducers.js
@@ -0,0 +1,8 @@
+const INITIALSTATE = {email: ''};
+
+export default (state = INITIALSTATE, action) => {
+    switch (action.type){
+        default: 
+            return state;
+    }
+};
