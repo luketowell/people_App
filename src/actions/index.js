8d40f58557e5edded9640ce46af617a724413367
Unknown <luke.towell@hotmail.com>
2018-08-17 15:40:21 +0100

    More action and reducer setup
    made more changes to the actions and the reducers.
    Set up the actions and reducers to use types instead of directly using strings.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+import {EMAIL_CHANGED, PASSWORD_CHANGED} from './types';
+
 export const emailChanged = (text) => {
     return {
-        type: 'emailChanged',
+        type: EMAIL_CHANGED,
         payload: text
     };
 };
--- src/reducers/authReducers.js
+++ src/reducers/authReducers.js
@@ -1,8 +1,17 @@
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from "../actions/types";
+
 const INITIALSTATE = {email: ''};
 
 export default (state = INITIALSTATE, action) => {
     switch (action.type){
+        EMAIL_CHANGED:
+            return state
+            break;
+        PASSWORD_CHANGED:
+            return state
+            break;
         default: 
             return state;
+            break;
     }
 };
