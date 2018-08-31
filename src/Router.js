61ee30e6523ec0fa92ebd194b8eaa0acc8986e95
Unknown <luke.towell@hotmail.com>
2018-08-31 15:23:55 +0100

    added a new view and added in a scene for create new employee
    added a new view and added in a scene for create new employee

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router } from 'react-native-router-flux';
 import LoginForm  from './components/LoginForm';
 import List from './components/List';
 import Detail from './components/Detail';
+import CreateEmployee from './components/CreateEmployee';
 
 const RouterComponent = () => {
     return (
@@ -12,7 +13,14 @@ const RouterComponent = () => {
                     <Scene key="login" component={LoginForm} title="Please Login" initial/> 
                 </Scene>
                 <Scene key="main">
-                    <Scene key="List" component={List} title="List"/>
+                    <Scene 
+                        rightTitle="Add"
+                        onRight={() => {console.log("clicked mother fucker")}}
+                        key="List" 
+                        component={List} 
+                        title="List"
+                    />
+                    <Scene Key="NewEmployee" component={CreateEmployee} title="Add Employee"/>
                     <Scene key="Detail" component={Detail} title="Detail"/>
                 </Scene>   
             </Scene>
---
+++ src/components/CreateEmployee.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Spinner, Button } from './common';
+
+class CreateEmployee extends Component {
+
+    render(){
+        <Card>
+            <CardSection>
+                <Button buttonText="Button"/>
+            </CardSection>
+        </Card>
+    }
+}
